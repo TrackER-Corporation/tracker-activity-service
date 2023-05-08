3d4c75912404a2fb69b8f54ff9a9457d0c011eec
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-08 17:41:25 +0200

    fix: add error on not found

--- db/controller/controller.ts
+++ db/controller/controller.ts
@@ -1,14 +1,12 @@
 import axios from 'axios';
 import asyncHandler from 'express-async-handler';
 import { collections } from '../services/database.service';
-const ObjectId = require("mongodb").ObjectId;
+import { ObjectId } from 'mongodb';
 
 export const createActivity = asyncHandler(async (req, res) => {
     const { userId } = req.body
-    if (!userId) {
-        res.status(400)
-        return
-    }
+    if (!userId) throw Error('Error')
+
     const data = await axios.get('https://geolocation-db.com/json/').then((data) => data.data)
     const activityExists = await collections?.activity?.findOne({ IPv4: data.IPv4 })
     if (activityExists) {
@@ -20,15 +18,15 @@ export const createActivity = asyncHandler(async (req, res) => {
             res.status(200).json({ ...data, date: Date.now() })
     }
     else {
-        const activity = await collections?.activity?.insertOne({
-            ...data,
-            userId: new ObjectId(userId)
-        })
+        const activity = await collections?.activity?.
+            insertOne({
+                ...data,
+                userId: new ObjectId(userId)
+            })
         if (activity) {
             res.status(200).json({ ...data })
-        } else {
-            res.status(400)
-        }
+        } else
+            throw Error('Error')
     }
 
 })
@@ -38,8 +36,8 @@ export const createActivity = asyncHandler(async (req, res) => {
 // @access  Private
 export const getActivityById = asyncHandler(async (req, res) => {
     const activity = await collections?.activity?.find({ userId: new ObjectId(req.params.id) }).toArray();
-    if (activity?.length == 0) {
-        res.status(401)
+    if (!activity || activity?.length == 0) {
+        throw Error('Error')
     } else {
         res.status(200)
     }
