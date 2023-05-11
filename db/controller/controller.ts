9f0ff5eaf2188e371bcc4986e35d403f59dbfe3d
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-11 14:37:01 +0200

    fix(getActivityById): return array

--- db/controller/controller.ts
+++ db/controller/controller.ts
@@ -7,15 +7,21 @@ export const createActivity = asyncHandler(async (req, res) => {
     const { userId } = req.body
     if (!userId) throw Error('Error')
 
-    const data = await axios.get('https://geolocation-db.com/json/').then((data) => data.data)
-    const activityExists = await collections?.activity?.findOne({ IPv4: data.IPv4 })
+    const data = await axios.get('https://geolocation-db.com/json/')
+        .then((data) => data.data)
+        .catch(err => { throw Error(err) })
+
+    const activityExists = await collections.activity?.findOne({ IPv4: data.IPv4 })
+
     if (activityExists) {
         const updatedUser = await collections?.activity?.findOneAndUpdate(
             { userId: new ObjectId(activityExists.userId) },
             { $set: { ...data, date: Date.now() } },
             { returnDocument: 'after' })
-        if (updatedUser)
+        if (updatedUser) {
+            console.log(updatedUser)
             res.status(200).json({ ...data, date: Date.now() })
+        }
     }
     else {
         const activity = await collections?.activity?.
@@ -35,10 +41,8 @@ export const createActivity = asyncHandler(async (req, res) => {
 // @route   GET /activity/:id
 // @access  Private
 export const getActivityById = asyncHandler(async (req, res) => {
-    const activity = await collections?.activity?.find({ userId: new ObjectId(req.params.id) }).toArray();
-    if (!activity || activity?.length == 0) {
-        throw Error('Error')
-    } else {
-        res.status(200)
-    }
+    collections.activity?.find({ userId: new ObjectId(req.params.id) }).toArray().then(activity => {
+        console.log(activity)
+        res.status(200).json(activity)
+    }).catch(e => { throw Error('Error') });
 })
